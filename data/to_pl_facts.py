cce14c38666262f1b0cd3165b07dc19eea94c3cd
Stefano <stefano.forti92@gmail.com>
2025-08-29 14:31:56 +0100

    carbon intensity data

---
+++ data/to_pl_facts.py
@@ -0,0 +1,11 @@
+import csv
+
+open_file = open("./Carbon_Intensity_Data.csv", "r")
+
+outputFile = open("./ci.pl", "w")
+
+for line in csv.DictReader(open_file):
+    timeslot = line["Timeslot"]
+    region = line["Region"].lower().strip().replace(" ", "_")
+    carbon_intensity = line["Value"]
+    outputFile.write(f'carbonIntensity("{timeslot}", {carbon_intensity}, {region}).\n')
